a2d617a5f1c3c2f3fce07f81c99eb8acd9855904
corerd <corerd@live.com>
2015-12-30 15:55:36 +0100

    Add debug switch in cfg_file_arg function

--- utils/cli.py
+++ utils/cli.py
@@ -28,9 +28,13 @@
 
 
 def cfg_file_arg(prog_version, prog_usage, default_cfg_file):
-    '''Parse cli arguments matching -c or --cfg option.
+    '''Parse cli arguments matching the following options:
+         -h or --help
+         -c or --cfg
+         -d or --debug
+         -v or --version
 
-    Returns args.cfg_file.
+    Returns args
     '''
     from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -42,6 +46,9 @@ def cfg_file_arg(prog_version, prog_usage, default_cfg_file):
     parser.add_argument('-c', '--cfg', dest='cfg_file',
                             help=desc,
                             default=default_cfg_file)
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true',
+                            help='enable debug',
+                            default='False')
     parser.add_argument('-v', '--version', action='version',
                         version='%%(prog)s %s' % prog_version)
     args = parser.parse_args()
@@ -50,4 +57,8 @@ def cfg_file_arg(prog_version, prog_usage, default_cfg_file):
 
 if __name__ == "__main__":
     options = cfg_file_arg('X.Y', 'Usage string', 'default_cfg_file')
-    print options.cfg_file
+    print 'Configuration file:', options.cfg_file
+    if options.debug is True:
+        print 'Debug is Enable'
+    else:
+        print 'Debug is Disable'
