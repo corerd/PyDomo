e1a7bd4b4f0783a64a510640f8b37eaf1b275588
corerd <corerd@live.com>
2016-01-03 18:10:40 +0100

    Add Jinja2 template support to web server Bootstrap skeleton

--- web/bootstrap-starter.py
+++ web/bootstrap-starter.py
@@ -40,6 +40,7 @@ python bootstrap-starter.py
 
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
+from jinja2 import Environment, PackageLoader, TemplateNotFound
 from os import curdir, sep
 import ssl
 import base64
@@ -50,6 +51,29 @@ STATIC_FILE_DIR = 'static'
 '''GLOBALS'''
 auth_key = ""
 static_endpoint = curdir + sep + STATIC_FILE_DIR
+
+'''Create an instance of the JINJA Environment class:
+http://jinja.pocoo.org/docs/dev/api/#basics
+
+The loader looks up the templates in the templates folder
+inside the application’s python package.
+
+See also http://stackoverflow.com/a/6196098
+and http://stackoverflow.com/a/9763688
+
+If you are using a single module (as in this example),
+__name__ is always the correct value.
+If you however are using a package, it’s usually recommended
+to hardcode the name of your package there.
+
+The assignment below is module / package independent:
+'''
+app_package = __name__.split('.')[0]
+jinja_env = Environment(
+    loader=PackageLoader(app_package, 'templates'),
+    trim_blocks=True,
+    lstrip_blocks=True,
+    extensions=['jinja2.ext.autoescape'])
 
 
 class AuthHandler(SimpleHTTPRequestHandler):
@@ -84,17 +108,28 @@ class AuthHandler(SimpleHTTPRequestHandler):
             self.wfile.write(self.headers.getheader('Authorization'))
             self.wfile.write('not authenticated')
 
+    def do_mimetype_HEAD(self, mimetype):
+        '''send header according to mimetype'''
+        self.send_response(200)
+        self.send_header('Content-type', mimetype)
+        self.end_headers()
+
     def webpage_handle(self):
         '''Handler for any other incoming request after authentication.
 
         Derived from: http://www.acmesystems.it/python_httpd
         '''
         if self.path == "/":
-            self.path = "/bootstrap-starter.html"
+            self.path = "/bootstrap-starter.htm"
 
         #Check the file extension required and
         #set the right mime type
         sendReply = False
+        is_jinja_template = False
+        if self.path.endswith(".htm"):
+            mimetype = 'text/html'
+            is_jinja_template = True
+            sendReply = True
         if self.path.endswith(".html"):
             mimetype = 'text/html'
             sendReply = True
@@ -115,17 +150,24 @@ class AuthHandler(SimpleHTTPRequestHandler):
             sendReply = True
 
         if sendReply is True:
-            #Open the static file requested and send it
-            static_file_path = static_endpoint + self.path
-            try:
-                static_file = open(static_file_path)
-                self.send_response(200)
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                self.wfile.write(static_file.read())
-                static_file.close()
-            except IOError:
-                self.send_error(404, 'File Not Found: %s' % self.path)
+            if is_jinja_template is True:
+                #Render the template file and send it
+                try:
+                    template = jinja_env.get_template(self.path)
+                    self.do_mimetype_HEAD(mimetype)
+                    self.wfile.write(template.render(proj_name=app_package))
+                except TemplateNotFound as e:
+                    self.send_error(404, 'Template Not Found: %s' % e.name)
+            else:
+                #Open the static file requested and send it
+                static_file_path = static_endpoint + self.path
+                try:
+                    static_file = open(static_file_path)
+                    self.do_mimetype_HEAD(mimetype)
+                    self.wfile.write(static_file.read())
+                    static_file.close()
+                except IOError:
+                    self.send_error(404, 'File Not Found: %s' % self.path)
 
 
 def start_server(user, password, ssl_certificate, server_key):
