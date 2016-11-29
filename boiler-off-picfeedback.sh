efe59be6aba36926c7487d7ef800f6a5eb53c71c
Corrado Ubezio <corerd@live.com>
2016-11-29 05:39:42 +0100

    Move to the working directory of the bash script

--- boiler-off-picfeedback.sh
+++ boiler-off-picfeedback.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Move to the working directory of the bash script
+# See: http://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 ./camsnapshot.py
 usbrelay UWMGH_1=0 2>/dev/null
 sleep 3
