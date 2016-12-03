c59fbe69b492d0d7eed2cc683bf750e1cfb94816
Corrado Ubezio <corerd@live.com>
2016-12-03 18:15:41 +0100

    Rewriting

--- cloud/weather.py
+++ cloud/weather.py
@@ -22,58 +22,147 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import print_function
+
 import logging
+import time
 import urllib2
 import json
 import sys
-from os.path import join
+from os import mkdir
+from os.path import dirname, join, isdir, realpath
 
-from cloudcfg import ConfigDataLoad
+
+# Globals
+VERSION = '1.0'
+VERSION_DATE = '2016'
+DEFAULT_CFG_FILE = 'cloudcfg.json'
+
+DEFAULT_CFG_FILE_PATH = join(dirname(realpath(__file__)), DEFAULT_CFG_FILE)
+
+USAGE = '''Get the location current temperature in Celsius degrees.
+
+Data are logged in CSV format: datetime;city;temperature
+
+Location and datastore path are taken from a configuration file in JSON format.
+If none given, the configuration is read from the file:
+    %s
+''' % DEFAULT_CFG_FILE_PATH
 
 
-def wu_getConditions(log_file, user_api_key, country, city):
+def print_error(msg):
+    print('%s;%s' % (time.strftime("%Y-%m-%d %H:%M:%S"), msg), file=sys.stderr)
+
+
+def check_directory(dir_name):
+    """If the directory dir_name doesn't exist,
+    then create it.
+    Returns True for SUCCESS, False otherwise
+    """
+    exists = True
+    if not isdir(dir_name):
+        try:
+            # Creates the directory in a secure manner
+            mkdir(dir_name, 0700)
+        except OSError:
+            if not isdir(dir_name):
+                # If the directory doesn't exist,
+                # there was an error on creation
+                exists = False
+    return exists
+
+
+def wu_getConditions(user_api_key, country, city):
     """Get the current temperature in Celsius degrees using Weather Underground API.
     Weather Underground home: "https://www.wunderground.com/weather/api
 
-    Data are logged in CSV format: datetime;city;temperature
-    Return value is 0 for SUCCESS, -1 otherwise
+    Return tuple (city, temperature), empty tuple if rised some errors.
     """
     wu_request_url = 'http://api.wunderground.com/api/%s/geolookup/conditions/q/%s/%s.json' % \
                                                 ( user_api_key, country, city )
-    logging.basicConfig(filename=log_file,
-                        format='%(asctime)s %(levelname)s;%(message)s',
-                        level=logging.DEBUG)
     try:
         f = urllib2.urlopen(wu_request_url)
         json_string = f.read()
         f.close()
     except:
         # See: http://stackoverflow.com/a/4990739
-        logging.error("urllib2 error;%s" % sys.exc_info()[0])
-        return -1
+        print_error("WU: urllib2 error;%s" % sys.exc_info()[0])
+        return ()
     try:
         parsed_json = json.loads(json_string)
         location = parsed_json['location']['city']
         temp_c = parsed_json['current_observation']['temp_c']
     except:
         # See: http://stackoverflow.com/a/4990739
-        logging.error("parse json error;%s" % sys.exc_info()[0])
+        #print(json_string, file=sys.stderr)
+        print_error("WU: parse json error;%s" % sys.exc_info()[0])
+        return ()
+    try:
+        f_temp_c = float(temp_c)
+    except:
+        # See: http://stackoverflow.com/a/4990739
+        print_error("WU: string convertion to float error;%s" % sys.exc_info()[0])
+        return ()
+    return (location, f_temp_c)
+
+
+def getLocationTemp(user_api_key, country, city):
+    """Return tuple (city, temperature), empty tuple if rised some errors.
+    """
+    retries = 0
+    locationTemp = wu_getConditions(user_api_key, country, city)
+    while len(locationTemp) <= 0:
+        if retries >= 2:
+            break
+        retries = retries + 1
+        time.sleep(5)
+        locationTemp = wu_getConditions(user_api_key, country, city)
+    return locationTemp
+
+
+def updateLog(user_api_key, country, city):
+    """Updates location temperature log in CSV format: datetime;city;temperature
+    Return value is 0 for SUCCESS, -1 otherwise
+    """
+    locationTemp = getLocationTemp(user_api_key, country, city)
+    if len(locationTemp) <= 0:
+        logging.error("getLocationTemp")
         return -1
-    logging.info("%s;%s" % (location, temp_c))
+    logging.info("%s;%0.1f" % (locationTemp[0], locationTemp[1]))
     return 0
 
+
 def main():
-    cfg = ConfigDataLoad("cloudcfg.json")
+    from utils.cli import cfg_file_arg
+    from cloudcfg import ConfigDataLoad
+
+    options = cfg_file_arg(VERSION, USAGE, DEFAULT_CFG_FILE_PATH, VERSION_DATE)
+    print('Read configuration from file: %s' % options.cfg_file)
+
+    try:
+        cfg = ConfigDataLoad(options.cfg_file)
+    except:
+        print('Unable to load config', file=sys.stderr)
+        return -1
+
+    if check_directory(cfg.data['datastore']) is not True:
+        print("Cannot access %s directory" % cfg.data['datastore'], file=sys.stderr)
+        return -1
+
     log_file = join(cfg.data['datastore'], "temperature.txt")
-    return wu_getConditions(log_file,
-                            cfg.data['wu-api-key'],
-                            cfg.data['wu-search-country'],
-                            cfg.data['wu-search-city'])
+    logging.basicConfig(filename=log_file,
+                        format='%(asctime)s;%(levelname)s;%(message)s',
+                        level=logging.DEBUG)
+
+    status = updateLog( cfg.data['wu-api-key'],
+                        cfg.data['wu-search-country'],
+                        cfg.data['wu-search-city'] )
+    if status == 0:
+        print('Temperature log in file: %s' % log_file)
+    else:
+        print('FAIL!')
+    return status
+
 
 if __name__ == "__main__":
-    status = main()
-    if status == 0:
-        print 'Done!'
-    else:
-        print 'FAIL!'
-    exit(status)
+    exit(main())
