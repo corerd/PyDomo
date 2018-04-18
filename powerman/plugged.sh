7bd301abd79fa5e633c296f6c4b516709c0c70b7
Corrado Ubezio <corerd@live.com>
2018-04-18 05:28:42 +0200

    Add error checks

--- powerman/plugged.sh
+++ powerman/plugged.sh
@@ -6,7 +6,16 @@
 # Instead of checking for AC adapter (not working on my architecture)
 # check for battery status
 
-battery_status=$(acpi -b | cut -d' ' -f3 | cut -d, -f1)
+#battery_status=$(acpi -b | cut -d' ' -f3 | cut -d, -f1)
+battery_status=$(acpi -b)
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+battery_status=$(echo $battery_status | cut -d' ' -f3 | cut -d, -f1)
+if [ "$battery_status" = "" ]; then
+    echo "No battery informations" 1>&2
+    exit 2
+fi
 if [ "$battery_status" = "Discharging" ]; then
     echo "The AC Adapter is OFF"
 else
